e409385b7e305536c5f9d34806a386b37c4896fc
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-05-29 20:51:51 +0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
+};
